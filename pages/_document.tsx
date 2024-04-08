5d5e9fbcde8d2f129973f70d39687ce7f791d438
Mark Fazzio <mark@pixelpandas.com>
2024-04-08 15:13:30 -0400

    rubik font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
           rel="stylesheet"
         />
       </Head>
